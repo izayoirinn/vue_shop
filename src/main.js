fde77d93a9d9a816d3056c9ed195201a7ae8ee6b
izayoirinn-location <2022746606@qq.com>
2021-07-20 11:44:58 +0800

    登录框布局完成

--- src/main.js
+++ src/main.js
@@ -3,17 +3,10 @@ import App from './App.vue'
 import router from './router/index'
 // 导入全局样式表
 import './assets/css/global.css';
+// 引入elementUI
+import './element/index.js';
 Vue.config.productionTip = false
-// 按需引入element-ui
-import {
-  Button,
-  Select
-} from 'element-ui';
-/* Vue.component(Button.name, Button);
-Vue.component(Select.name, Select); */
 
-Vue.use(Button);
-Vue.use(Select);
 new Vue({
   render: h => h(App),
   router
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="login_container">登录组件</div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 登录头像区  -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 表单区 -->
+      <el-form class="login_form">
+        <!-- 用户名 -->
+        <el-form-item >
+          <el-input prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item >
+          <el-input prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+        <!-- 按钮区 -->
+         <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,6 +35,46 @@ export default {
 .login_container {
   background-color: #2b4b6b;
   height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
-</style>>
+.login_box {
+  position: relative;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    margin: -65px auto;
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+
+      background-color: #eee;
+    }
+  }
+}
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  // https://developer.mozilla.org/zh-CN/docs/Web/CSS/box-sizing
+  box-sizing: border-box;
+}
+.btns{
+  display: flex;
+  justify-content: flex-end;
+
+}
+</style>
 
